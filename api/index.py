f3a288aec553510af5e664c65155674e378a27dc
Ross Freedman <rossfreedman@mymacbook.local>
2024-11-23 15:10:02 -0600

    Simplify app for testing

--- api/index.py
+++ api/index.py
@@ -1,16 +1,9 @@
-from flask import Flask, jsonify, request, render_template
-import pandas as pd
-import random
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
-# ... rest of your code ...
+@app.route('/')
+def home():
+    return "Hello, World!"
 
-# Change the last part to:
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return render_template('index.html')
-
-# Remove the if __name__ == '__main__' block 
+# Keep only this basic route for now to test deployment 
